fbe52b058269bf5bd99494156694a9f13ad85737
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-26 21:44:26 -0300

    Add migration.

---
+++ database/migrations/2023_06_26_214209_create_tots_page_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tots_page', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80)->nullable(false);
+            $table->string('slug', 80)->nullable(false);
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+
+            $table->foreign('language_id')->references('id')->on('tots_language');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tots_page');
+    }
+};
