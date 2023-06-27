9345af18a3087d9dd313070107a9c263838f1675
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-26 21:51:59 -0300

    Add list controller.

---
+++ src/Http/Controllers/ListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tots\Page\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tots\Page\Models\TotsPage;
+
+class ListController extends \Laravel\Lumen\Routing\Controller
+{
+    public function handle(Request $request)
+    {
+        // Create query
+        $elofilter = \Tots\EloFilter\Query::run(TotsPage::class, $request);
+        // Custom filters
+        //$elofilter->getQueryRequest()->addWhere('id', 2);
+        // Execute query
+        return $elofilter->execute();
+    }
+}
